a87c00b2b3bcd38898a8e8134212add4d2a3d85b
Vadym <Maslovskyi>
2024-05-02 22:32:09 +0300

    update root route

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,5 +6,8 @@ const router = new Router()
 router.post('/upload', calculation.upload)
 router.get('/calculations', calculation.calculations)
 router.get('/calculation', calculation.calculation)
+router.get('/', (req, res) => {
+    res.send('Hello world')
+})
 
 export default router
